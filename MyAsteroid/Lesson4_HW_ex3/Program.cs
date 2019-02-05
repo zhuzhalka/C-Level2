9dfed1288e3770f71e626b204131bf912e3b02b2
zhuzhalka <drob.konst@mail.ru>
2019-02-05 16:47:57 +0300

    hw4

---
+++ MyAsteroid/Lesson4_HW_ex3/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson4_HW_ex3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Dictionary<string, int> dict = new Dictionary<string, int>()
+                {
+                    { "four" , 4 },
+                    { "two" , 2 },
+                    { "one" , 1 },
+                    { "three" , 3 },
+                };
+            //var d = dict.OrderBy(delegate (KeyValuePair<string, int> pair) {return pair.Value;});
+            
+           var d = dict.OrderBy => 
+
+            foreach (var pair in d)
+            {
+                Console.WriteLine("{0} - {1}", pair.Key, pair.Value);
+            }
+            Console.ReadLine();
+        }
+    }
+}
