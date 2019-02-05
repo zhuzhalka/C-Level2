8f0b473ad4e4bf525679200fde97955a917408c5
zhuzhalka <drob.konst@mail.ru>
2019-02-05 16:46:56 +0300

    hw4

---
+++ MyAsteroid/MyAsteroid/Health.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace MyAsteroid
+{
+    class Health : BaseObject
+    {
+        public Health(Point Pos, Point Dir, Size size) : base(Pos, Dir, size)
+        {
+        }
+       
+        public override void Draw()
+        {
+            Game.Buffer.Graphics.DrawLine(Pens.Green, Pos.X, Pos.Y, Pos.X+10, Pos.Y);
+            Game.Buffer.Graphics.DrawLine(Pens.Green, Pos.X+5, Pos.Y-5, Pos.X + 5, Pos.Y+5);
+        }
+        Random rnd = new Random();
+        public override void Update()
+        {
+            Pos.X = Pos.X + Dir.X;
+            if (Pos.X < 0)
+            {
+                Pos.X = Game.Width + Size.Width;
+                Pos.Y = rnd.Next(0, 900);
+            }
+        }
+
+        public override void ColUpdate()
+        {
+            Pos.X = Game.Width + Size.Width;
+            Pos.Y = rnd.Next(0, 900);
+        }
+    }
+}
---
+++ MyAsteroid/MyAsteroid/Ship.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace MyAsteroid
+{
+    class Ship : BaseObject
+    {
+        private int _energy = 100;
+        public int Energy => _energy;
+        public int score;
+
+        public void EnergyLow(int n)
+        {
+            _energy -= n;
+        }
+
+        public Ship(Point Pos, Point Dir, Size size) : base(Pos, Dir, size)
+        {
+        }
+
+        public void ScorePlus()
+        {
+            score = score + 10;
+        }
+
+        public void ScoreMinus()
+        {
+            score = score - 30;
+        }
+
+        public override void Draw()
+        {
+            Game.Buffer.Graphics.FillEllipse(Brushes.Wheat, Pos.X, Pos.Y, Size.Width, Size.Height);
+        }
+        public override void Update()
+        {
+        }
+
+        public void Up()
+        {
+            if (Pos.Y > 0) Pos.Y = Pos.Y - Dir.Y;
+        }
+
+        public void Down()
+        {
+            if (Pos.Y < Game.Height) Pos.Y = Pos.Y + Dir.Y;
+        }
+              
+        public static event Message MessageDie;
+        public void Die()
+        {
+            MessageDie?.Invoke();
+        }
+
+        public override void ColUpdate()
+        {
+            _energy = _energy + 10;
+        }
+    }
+}
